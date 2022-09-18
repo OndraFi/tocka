955e11503fca34780e0bdfcf8d4769328ae02e4a
OndraFi <111919985+OndraFi@users.noreply.github.com>
2022-09-18 18:21:32 +0200

    delete btn

--- tocky/src/formItem.js
+++ tocky/src/formItem.js
@@ -3,12 +3,12 @@ import randomColor from "randomcolor";
 
 export default function FormItem(props){
     const pocet = props.pocet;
-    const [text,setText] = useState(props.item.text);
-    const [color,setColor] = useState(props.item.color);
+    const color = hexRgb(props.item.color);
+
     return(
-        <div className={"d-flex justify-content-around align-items-center shadow p-5"}>
-            <label className={"mx-1 mx-md-5"}>Barva</label><input id={`color${pocet}`} type={"color"} value={color} onChange={(e)=>{props.item.color = e.target.value; setColor(e.target.value);}} className={"w-100"}/>
-            <label className={"mx-1 mx-md-5"}>nazev:</label><input id={`nazev${pocet}`}  type={"text"} value={text} onChange={(e)=>{props.item.text = e.target.value;setText(e.target.value)}} className={"w-100"}/>
+        <div className={"d-flex justify-content-around align-items-center p-5 my-3 mx-2"} style={{}}>
+            <label className={"mx-1 mx-md-5"}>Barva</label><input id={`color${pocet}`} type={"color"} value={props.item.color} onChange={(e)=>{props.colorChange(pocet,e)}} className={"w-100"}/>
+            <label className={"mx-1 mx-md-5"}>nazev:</label><input id={`nazev${pocet}`}  type={"text"} value={props.item.text} onChange={(e)=>{props.textChange(pocet,e)}} className={"w-100"}/>
             <div onClick={()=>{props.delete(pocet)}} className={"btn btn-danger mx-2"}>delete</div>
         </div>
     );
