2801d51d8f000eb133f6f68c65feac6ad84b11c2
OndraFi <111919985+OndraFi@users.noreply.github.com>
2022-09-18 18:34:08 +0200

    delete btn

--- tocky/src/formItem.js
+++ tocky/src/formItem.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function FormItem(props){
     const pocet = props.pocet;
 
     return(
-        <div className={"d-flex justify-content-around align-items-center p-5 my-3 mx-2"} style={{}}>
+        <div className={"d-flex justify-content-around align-items-center shadow-lg p-5 my-3 mx-2"}>
             <label className={"mx-1 mx-md-5"}>Barva</label><input id={`color${pocet}`} type={"color"} value={props.item.color} onChange={(e)=>{props.colorChange(pocet,e)}} className={"w-100"}/>
             <label className={"mx-1 mx-md-5"}>nazev:</label><input id={`nazev${pocet}`}  type={"text"} value={props.item.text} onChange={(e)=>{props.textChange(pocet,e)}} className={"w-100"}/>
             <div onClick={()=>{props.delete(pocet)}} className={"btn btn-danger mx-2"}>delete</div>
