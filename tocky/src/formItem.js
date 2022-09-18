7be22cecad81c96171364823398831153e6d155f
OndraFi <111919985+OndraFi@users.noreply.github.com>
2022-09-18 20:09:48 +0200

    delete btn

--- tocky/src/formItem.js
+++ tocky/src/formItem.js
@@ -8,6 +8,7 @@ export default function FormItem(props){
             <label className={"mx-1 mx-md-5"}>Barva</label><input id={`color${pocet}`} type={"color"} value={props.item.color} onChange={(e)=>{props.colorChange(pocet,e)}} className={"w-100"}/>
             <label className={"mx-1 mx-md-5"}>nazev:</label><input id={`nazev${pocet}`}  type={"text"} value={props.item.text} onChange={(e)=>{props.textChange(pocet,e)}} className={"w-100"}/>
             <div onClick={()=>{props.delete(pocet)}} className={"btn btn-danger mx-2"}>delete</div>
+            <span>nvm</span>
         </div>
     );
 }
