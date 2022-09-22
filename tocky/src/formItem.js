63530c0d8914954a9afb7aaff45a5662c93c4bc8
ondrafi <fialka.ondra@gmail.com>
2022-09-22 11:48:05 +0200

    tocka

--- tocky/src/formItem.js
+++ tocky/src/formItem.js
@@ -6,9 +6,11 @@ export default function FormItem(props){
     return(
         <div className={"d-flex justify-content-around align-items-center shadow-lg p-5 my-3 mx-2"}>
             <div>
+            <div>
             <label className={"mx-1 mx-md-5"}>Barva:</label><input id={`color${pocet}`} type={"color"} value={props.item.color} onChange={(e)=>{props.colorChange(pocet,e)}} className={"w-100"}/>
             <label className={"mx-1 mx-md-5"}>nazev:</label><input id={`nazev${pocet}`}  type={"text"} value={props.item.text} onChange={(e)=>{props.textChange(pocet,e)}} className={"w-100"}/>
             <div onClick={()=>{props.delete(pocet)}} className={"btn btn-danger mx-2"}>delete</div>
+            </div>
             </div>
         </div>
     );
