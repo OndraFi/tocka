edb7183536f82903f2f0b4793380310130448526
ondrafi <fialka.ondra@gmail.com>
2022-09-22 12:18:47 +0200

    tocka

--- tocky/src/formItem.js
+++ tocky/src/formItem.js
@@ -10,6 +10,7 @@ export default function FormItem(props){
             <label className={"mx-1 mx-md-5"}>nazev:</label><input id={`nazev${pocet}`}  type={"text"} value={props.item.text} onChange={(e)=>{props.textChange(pocet,e)}} className={"w-100"}/>
             <div onClick={()=>{props.delete(pocet)}} className={"btn btn-danger mx-2"}>delete</div>
             </div>
+            <span></span>
         </div>
     );
 }
